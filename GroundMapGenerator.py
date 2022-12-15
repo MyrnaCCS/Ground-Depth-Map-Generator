91bb47e9f6bd243e0423b8d79122c34ddfbb209c
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-12-14 20:22:23 -0600

    Give the possibility to output a map on distacnces or z

--- GroundMapGenerator.py
+++ GroundMapGenerator.py
@@ -2,86 +2,94 @@ import numpy as np
 
 
 class GroundMapGenerator(object):
-    def __init__(self, K, depth_max, width, height):
-        self.K = K #Intrisic Parameters
-        self.width = width #Camera intrisic parameter
-        self.height = height #Camera intrisic parameter
-        self.depth_max = depth_max
-    
+	def __init__(self, K, depth_max, width, height):
+		self.K = K #Intrisic Parameters
+		self.width = width #Camera intrisic parameter
+		self.height = height #Camera intrisic parameter
+		self.depth_max = depth_max
 
-    def read_transformation_matrix(self, transformation_matrix_path):
-        file_rows = open(transformation_matrix_path).readlines()
 
-        T = np.zeros((3, 4))
+	def read_transformation_matrix(self, transformation_matrix_path):
+		file_rows = open(transformation_matrix_path).readlines()
 
-        for i in range(3):
-            row = file_rows[i].split(' ')
-            for j in range(4):
-                T[i, j] = float(row[j])
-        
-        return T
-    
+		T = np.zeros((3, 4))
 
-    def compute_3D_coordinates(self, point_2D, P):
-        A = np.zeros((2, 2))
-        b = np.zeros((2, 1))
-        
-        # Rotation matrix dot K
-        R = P[:, :3]
-        # Translation vector dot K
-        t = P[:, 3]
-        
-        # Equations system type: Ax = b
-        A[0, 0] = (point_2D[0] * R[2, 0]) - R[0, 0]
-        A[0, 1] = (point_2D[0] * R[2, 1]) - R[0, 1]
-        A[1, 0] = (point_2D[1] * R[2, 0]) - R[1, 0]
-        A[1, 1] = (point_2D[1] * R[2, 1]) - R[1, 1]
-        b[0, 0] = t[0] - (point_2D[0] * t[2])
-        b[1, 0] = t[1] - (point_2D[1] * t[2])
-        
-        # Solution: A-1b = x
-        x = np.dot(np.linalg.inv(A), b)
-        
-        return x
-    
+		for i in range(3):
+			row = file_rows[i].split(' ')
+			for j in range(4):
+				T[i, j] = float(row[j])
 
-    def compute_depth_value(self, point_2D, T):
-        P = np.dot(self.K, T) # Projection matrix
-        
-        # Get 3D coordinates
-        point_3D = self.compute_3D_coordinates(point_2D, P)
-        
-        # Get R and t
-        R = T[:, :3]
-        t = T[:, 3]
-        
-        # Location of camera
-        R_inv = np.transpose(R)
-        cam = np.dot(R_inv, -t)
-        
-        # depth: 
-        depth_cam = np.sqrt((cam[0] - point_3D[0])**2 + (cam[1] - point_3D[1])**2 + (cam[2])**2)
-        
-        return depth_cam
-    
+		return T
 
-    def compute_ground_plane_depth_map(self, transformation_matrix_path, rows=160, cols=256):
-        T = self.read_transformation_matrix(transformation_matrix_path)
 
-        point_2D = np.ones((3, 1))
-        
-        # return depth map
-        depth_map = np.ones((rows, cols)) * 255
-        
-        for i in range(cols):
-            for j in range(rows):
-                point_2D[0] = (float(i) / cols) * self.width
-                point_2D[1] = (float(j) / rows) * self.height
-                depth = self.compute_depth_value(point_2D, T)
-                if depth <= self.depth_max:
-                    depth_map[rows-1-j, cols-1-i] = int(np.floor(depth / self.depth_max * 255.0))
-                else:
-                    depth_map[rows-1-j, cols-1-i] = 255
-                    break
-        
-        return depth_map
+	def compute_3D_coordinates(self, point_2D, P):
+		A = np.zeros((2, 2))
+		b = np.zeros((2, 1))
+
+		# Rotation matrix dot K
+		R = P[:, :3]
+		# Translation vector dot K
+		t = P[:, 3]
+
+		# Equations system type: Ax = b
+		A[0, 0] = (point_2D[0] * R[2, 0]) - R[0, 0]
+		A[0, 1] = (point_2D[0] * R[2, 1]) - R[0, 1]
+		A[1, 0] = (point_2D[1] * R[2, 0]) - R[1, 0]
+		A[1, 1] = (point_2D[1] * R[2, 1]) - R[1, 1]
+		b[0, 0] = t[0] - (point_2D[0] * t[2])
+		b[1, 0] = t[1] - (point_2D[1] * t[2])
+
+		# Solution: A-1b = x
+		x = np.dot(np.linalg.inv(A), b)
+
+		return np.insert(x,2,0.0,axis=0)
+
+
+	def compute_depth_value(self, point_2D, T):
+		# Projection matrix: from Wm to Wc
+		P = np.dot(self.K, T)
+
+		# Get 3D coordinates of points such that Zm = 0
+		point_3D = self.compute_3D_coordinates(point_2D, P)
+
+		# Get R and t: from Wm to Wc
+		R = T[:, :3]
+		t = T[:, 3]
+
+		# Location of camera in Wm
+		R_inv = np.transpose(R)
+		cam   = np.dot(R_inv, -t)
+
+		# Depth:
+		depth_cam = np.sqrt((cam[0] - point_3D[0])**2 + (cam[1] - point_3D[1])**2 + (cam[2])**2)
+		z_cam   = np.dot(R,point_3D-np.expand_dims(cam,-1))
+		return depth_cam, z_cam[2]
+
+
+	def compute_ground_plane_depth_map(self, transformation_matrix_path, rows, cols, compute_z=True):
+		T = self.read_transformation_matrix(transformation_matrix_path)
+
+		point_2D = np.ones((3, 1))
+
+		# return depth map
+		depth_map = np.ones((rows, cols)) * 255
+
+		for i in range(cols):
+			for j in range(rows):
+				point_2D[0] = self.width/2.0 + (float(i-cols/2.0) / cols) * self.width
+				point_2D[1] = self.height/2.0+ (float(j-rows/2.0) / rows) * self.height
+				depth, z = self.compute_depth_value(point_2D, T)
+				if compute_z==False:
+					if depth <= self.depth_max:
+						depth_map[rows-1-j, cols-1-i] = int(np.floor(depth / self.depth_max * 255.0))
+					else:
+						depth_map[rows-1-j, cols-1-i] = 255
+						break
+				else:
+					if z <= self.depth_max:
+						depth_map[rows-1-j, cols-1-i] = int(np.floor(z / self.depth_max * 255.0))
+					else:
+						depth_map[rows-1-j, cols-1-i] = 255
+						break
+
+		return depth_map
--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import cv2
 import numpy as np
-import os
+import os, re
 from glob import glob
 from GroundMapGenerator import GroundMapGenerator
 
 dataset_path = '/home/myrna/Desktop/Indoors_Dataset/data'
-
-sequences_dir = ['40_D', '41_D', '42_D', '43_D', '44_D', '45_D', '46_D', '47_D', '48_D', '49_D', '50_D', '51_D', '52_D', '53_D', '54_D', '55_D', '56_D', '57_D']
-
+dataset_path = '/home/jbhayet/opt/repositories/devel/Obstacle-detection-and-depth-estimation/data/INDOORS/'
+#sequences_dir = ['40_D', '41_D', '42_D', '43_D', '44_D', '45_D', '46_D', '47_D', '48_D', '49_D', '50_D', '51_D', '52_D', '53_D', '54_D', '55_D', '56_D', '57_D']
+sequences_dir = ['21_D','22_D','23_D','24_D','25_D','26_D','27_D','28_D','29_D','31_D','37_D']
+#sequences_dir = ['20_D']
 K = np.array([[616.4, 0., 319.], [0., 611.21, 239.], [0., 0., 1.]])
-
 depth_map_generator = GroundMapGenerator(K, depth_max=7.0, width=640, height=480)
 
 
 for seq in sequences_dir:
-    transformation_matrix_dirs = sorted(glob(os.path.join(dataset_path, seq, 'transformation_matrix', '*' + '.txt')))
-    
-    ground_map_dir = os.path.join(dataset_path, seq, 'ground')
-    
-    os.mkdir(ground_map_dir)
-    
-    for idx, matrix_path in enumerate(transformation_matrix_dirs):
-        depth_map = depth_map_generator.compute_ground_plane_depth_map(matrix_path, rows=80, cols=128)
-        cv2.imwrite(os.path.join(ground_map_dir, str(idx).zfill(5) + '.png'), depth_map)
+	transformation_matrix_dirs = sorted(glob(os.path.join(dataset_path, seq, 'transformation_matrix', '*' + '.txt')))
+
+	ground_map_dir = os.path.join(dataset_path, seq, 'ground_plane_z')
+
+	os.mkdir(ground_map_dir)
+
+	for idx, matrix_path in enumerate(transformation_matrix_dirs):
+		print(matrix_path)
+		n   = re.findall('\d+', matrix_path )
+		num = int(n[1])
+
+		depth_map = depth_map_generator.compute_ground_plane_depth_map(matrix_path, rows=160, cols=256)
+		cv2.imwrite(os.path.join(ground_map_dir, str(num).zfill(5) + '.png'), depth_map)
